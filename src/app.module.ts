c03ee4c14115955f6c2171d2d6d68eead55bc42e
David <davidpbarros96@gmail.com>
2026-03-23 15:28:10 -0300

    chore: setup QA dev environment and branches

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,10 @@ import { JwtAuthGuard } from './common/guards/jwt-auth.guard'
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.' + process.env.NODE_ENV, '.env'],
+    }),
     PrismaModule,
     AuthModule,
     EstoqueModule,
